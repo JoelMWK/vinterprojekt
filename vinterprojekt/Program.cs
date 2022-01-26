9edb727ab9fc63d6202ad636953b2a749fe4665a
Joel Kustemo <kustemo.joel@elev.ga.ntig.se>
2022-01-26 13:31:43 +0100

    commit

--- vinterprojekt/Program.cs
+++ vinterprojekt/Program.cs
@@ -1,52 +1,141 @@
 ﻿using System;
 using System.Collections.Generic;
 using Raylib_cs;
+using System.Numerics;
 
-
-int speed = 3;
-int speedJ = 16;
-float speedF = 6.4f;
-float v = 5f;
-int bulletS = 4;
-
-Raylib.InitWindow(800, 800, "Vinterprojekt");
-Raylib.SetTargetFPS(60);
-
-Rectangle playerRect = new Rectangle(50, 661, 100, 119);
-Rectangle floor = new Rectangle(0, 780, 800, 20);
-
-bool collision = Raylib.CheckCollisionRecs(playerRect, floor);
-Rectangle overlap = Raylib.GetCollisionRec(playerRect, floor);
-Texture2D playerImage = Raylib.LoadTexture("yes.png");
-
-
-
-while (!Raylib.WindowShouldClose())
+static void main()
 {
 
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE)) playerRect.y -= speedJ;
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) playerRect.y += speed;
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) playerRect.x += speed;
-    if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) playerRect.x -= speed;
+    float speed = 4.3f;
+
+    Vector2 playerMovement = new Vector2();
 
 
-    Console.WriteLine(playerRect.y);
+    Raylib.InitWindow(1000, 1000, "Vinterprojekt");
+    Raylib.SetTargetFPS(60);
 
+    Rectangle playerRect = new Rectangle(50, 661, 88, 100);
+    Rectangle box = new Rectangle(500, 400, 80, 80);
 
-    Raylib.BeginDrawing();
-
-    Raylib.ClearBackground(Color.SKYBLUE);
-
-    Raylib.DrawRectangleRec(floor, Color.WHITE);
-    Raylib.DrawRectangleRec(playerRect, Color.WHITE);
-    Raylib.DrawTexture(playerImage, (int)playerRect.x, (int)playerRect.y, Color.WHITE);
-    Raylib.DrawRectangleRec(overlap, Color.ORANGE);
+    bool collision = false;
+    Rectangle overlap = Raylib.GetCollisionRec(playerRect, box);
+    Texture2D playerImage = Raylib.LoadTexture("yes.png");
 
 
 
 
 
 
-    Raylib.EndDrawing();
+    while (!Raylib.WindowShouldClose())
+    {
+        playerMovement = PlayerMovement(speed);
+        playerRect.y += playerMovement.Y;
+        playerRect.x += playerMovement.X;
+
+        if (Raylib.CheckCollisionRecs(playerRect, box))
+        {
+            playerRect.y -= playerMovement.Y;
+        }
+
+        if (Raylib.CheckCollisionRecs(playerRect, box))
+        {
+            playerRect.x -= playerMovement.X;
+        }
+
+        PlayerCollision(playerRect, collision, box, overlap);
+        PlayerShooting();
+        LevelDesign();
+
+        Console.WriteLine("y = " + playerRect.y);
+        Console.WriteLine("x = " + playerRect.x);
+
+        Raylib.BeginDrawing();
+
+        Raylib.ClearBackground(Color.SKYBLUE);
+
+        //Raylib.DrawRectangleRec(playerRect, Color.WHITE);
+        Raylib.DrawTexture(playerImage, (int)playerRect.x, (int)playerRect.y, Color.WHITE);
+        Raylib.DrawRectangleRec(box, Color.WHITE);
+        Raylib.DrawRectangleRec(overlap, Color.ORANGE);
+
+        Raylib.EndDrawing();
+
+    }
+}
+
+static void PlayerCollision(Rectangle playerRect, bool collision, Rectangle box, Rectangle overlap)
+{
+
+    if ((playerRect.x + playerRect.width) >= Raylib.GetScreenWidth())
+    {
+        playerRect.x = Raylib.GetScreenWidth() - playerRect.width;
+    }
+    else if (playerRect.x <= 0)
+    {
+        playerRect.x = 0;
+    }
+
+    if ((playerRect.y + playerRect.height) >= Raylib.GetScreenHeight())
+    {
+        playerRect.y = Raylib.GetScreenHeight() - playerRect.height;
+    }
+    else if (playerRect.y <= 0)
+    {
+        playerRect.y = 0;
+    }
+
+
+    collision = Raylib.CheckCollisionRecs(playerRect, box);
+
+    if (collision)
+    {
+        overlap = Raylib.GetCollisionRec(playerRect, box);
+    }
+}
+
+
+static void PlayerShooting()
+{
+
+    Rectangle bullet = new Rectangle(20, 20, 15, 45);
+    Texture2D bulletImage = Raylib.LoadTexture("bullet.png");
+
+    if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON))
+    {
+        //Raylib.DrawRectangleRec(bullet, Color.WHITE);
+        Raylib.DrawTexture(bulletImage, (int)bullet.x, (int)bullet.y, Color.WHITE);
+    }
 
 }
+
+static void LevelDesign()
+{
+
+    int[,] level = {
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        };
+
+}
+
+static Vector2 PlayerMovement(float speed)
+{
+    Vector2 movement = Vector2.Zero;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) movement.Y = -speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_S)) movement.Y = +speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_D)) movement.X += speed;
+    if (Raylib.IsKeyDown(KeyboardKey.KEY_A)) movement.X -= speed;
+
+    return movement;
+}
+
+
+main();
